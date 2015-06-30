89bb7e06465e9cde1c0513c91bcc6cd03432dbd4
mkdevops <z076qsf@z.zgs.de>
2015-06-30 11:08:39 +0200

    SiteLocalIT.java

---
+++ src/test/com/kellner/devops/SiteLocalIT.java
@@ -0,0 +1,44 @@
+// Part III: Add a similar Java class under /src/test/java/com/kellner/devops/SiteLocalIT.java 
+
+ package com.kellner.devops;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
+
+public class SiteLocalIT {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new HtmlUnitDriver();
+    }
+
+    @Test
+    public void testLocal() {
+        driver.get("http://localhost:8088/all");
+        WebElement element = driver.findElement(By.xpath("//h2[1]"));
+        Assert.assertEquals("Hello World!", element.getText());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+    
+} 
+
+

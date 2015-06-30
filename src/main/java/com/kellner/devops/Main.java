ed05588d30dd863a07d2c9d52a81add2260607bf
mkdevops <z076qsf@z.zgs.de>
2015-06-30 09:29:24 +0200

    Main.java

---
+++ src/main/java/com/kellner/devops/Main.java
@@ -0,0 +1,36 @@
+// Add a similar class to /src/main/java/com/huettermann/all/Main.java
+
+ 
+package com.huettermann.all;
+
+/**
+ * Created by michaelhuettermann  
+ */
+public class Main {
+
+    //private constructor
+    private Main() {}
+    
+    //here the start
+    public static void main ( String[] args ) {
+        System.out.println("hallo");
+        System.out.println("hallo");
+        System.out.println("hallo");
+        
+        int a = 0;
+        int b = 0;
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+        int c = 0;
+        int d = 0;
+        
+        for (int i = 0; i < 10; i++) {  
+           // ...
+        }
+        
+    }
+
+} 

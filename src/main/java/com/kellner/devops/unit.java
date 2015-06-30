db6d1b0cc7949bcda30801e49f6369272e869224
mkdevops <z076qsf@z.zgs.de>
2015-06-30 09:34:12 +0200

    unit.java

---
+++ src/main/java/com/kellner/devops/unit.java
@@ -0,0 +1,22 @@
+// add a similar class: src/test/java/com/huettermann/all/TestMe.java/
+
+
+package com.huettermann.all;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMe {
+
+    @Test
+    public void aComplexComputation() {
+        String a = "a";
+        String b = "a";
+        assertEquals(a, b);
+    }
+    
+}    
+    
+    
+ 
